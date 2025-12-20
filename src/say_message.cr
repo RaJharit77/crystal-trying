1fcf688259f0f296ba1e84c2219b3eea3259f6fb
RaJharit77 <hei.rajo.2@gmail.com>
2025-12-20 16:21:34 +0300

    feat: Add server, say_hello, say_message, say_message_number files and update .gitignore

---
+++ src/say_message.cr
@@ -0,0 +1,9 @@
+message = "Hello crush xD"
+
+puts message
+
+p! typeof(message) # type => string 
+
+crystal_message = "Hello Crystal"
+
+p! crystal_message
---
+++ src/say_message_number.cr
@@ -0,0 +1,7 @@
+message = "Hello Penny!"
+
+p! message, typeof(message)
+
+message = 73
+
+p! message, typeof(message)
